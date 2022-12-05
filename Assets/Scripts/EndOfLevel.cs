804549523e1c9691f6b07018fc6c96ade2ed5c30
Tatsiana <tptushko@my.centennialcollege.ca>
2022-12-05 17:08:52 -0500

    Level 2 added enemies

---
+++ Assets/Scripts/EndOfLevel.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndOfLevel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {            
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
