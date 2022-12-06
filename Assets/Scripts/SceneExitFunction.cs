48562305707ef96164ab9410a453c15c1ef3179d
Tatsiana <tptushko@my.centennialcollege.ca>
2022-12-05 21:55:43 -0500

    Fixed playtest errors

--- Assets/Scripts/SceneExitFunction.cs
+++ Assets/Scripts/SceneExitFunction.cs
@@ -5,10 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class SceneExitFunction : MonoBehaviour
 {
+   
+    public GameObject player;
+   
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
@@ -16,6 +20,7 @@ public class SceneExitFunction : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            Destroy(player);
             SceneManager.LoadScene("MainMenu");
         }
     }
